d214a9640f57ee1ea17deb074ee203a3c7f40ee6
Shiva33987 <kshivaprasad33987@gmail.com>
2026-02-12 23:24:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const roles = [
+  "Data Analyst",
+  "Python Developer",
+  "Machine Learning Enthusiast"
+];
+
+let index = 0;
+let charIndex = 0;
+const typingElement = document.querySelector(".typing");
+
+function type() {
+  if (charIndex < roles[index].length) {
+    typingElement.textContent += roles[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingElement.textContent =
+      roles[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    index = (index + 1) % roles.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", type);
